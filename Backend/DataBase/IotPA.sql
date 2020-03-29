afd44d671f19d03fc4f58b620cd1c6ff621ef405
FerEnriquez <feer.hernandez00@gmail.com>
2020-03-29 16:23:57 -0600

    Procedimiento almacenado para insertar un usuario nuevo

--- Backend/DataBase/IotPA.sql
+++ Backend/DataBase/IotPA.sql
@@ -15,3 +15,12 @@ BEGIN
 SELECT * FROM user WHERE username = username;
 END ;;
 DELIMITER ;
+
+--Insertar un usuario 
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insertUser`
+(IN username VARCHAR(20), IN name VARCHAR(45), IN email VARCHAR(45), IN encriptado VARCHAR(255), admin VARCHAR(20))
+INSERT INTO user 
+(username, name, email, password, status, date_register, date_unsubscribe, admin_id_admin) 
+VALUES(username, name, email, encriptado, TRUE, NULL, NULL, admin) ;;
+DELIMITER ;;
