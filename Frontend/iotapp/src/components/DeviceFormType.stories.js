423f58d2b1efa826ab45f54d6f7c4057c67e3f58
FerEnriquez <feer.hernandez00@gmail.com>
2020-05-01 12:19:08 -0500

    Type form for device added

--- Frontend/iotapp/src/components/DeviceFormType.stories.js
+++ Frontend/iotapp/src/components/DeviceFormType.stories.js
@@ -3,17 +3,16 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
 
-import DeviceFormName from './DeviceFormName';
+import DeviceFormType from './DeviceFormType';
 
-export const deviceFormName = {
-  name: 'Name Device',
+export const deviceFormType = {
+  type: 'Type Device',
   updatedAt: new Date(2018, 0, 1, 9, 0),
 };
 
 export const actions = {
-  repeteadName: action('repeatedName'),
+  
 };
 
-storiesOf('Form add: Name', module)
-  .add('default', () => <DeviceFormName deviceFormName={deviceFormName} {...actions} />)
-  
+storiesOf('Form add: Type', module)
+  .add('default', () => <DeviceFormType deviceFormType={deviceFormType} {...actions} />);
