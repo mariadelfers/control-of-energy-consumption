8928b8f400be8f888b517fb725ac35bfef9a1da8
FerEnriquez <feer.hernandez00@gmail.com>
2020-05-01 12:12:43 -0500

    Name form for device

---
+++ Frontend/iotapp/src/components/DeviceFormName.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
+
+import DeviceFormName from './DeviceFormName';
+
+export const deviceFormName = {
+  name: 'Name Device',
+  updatedAt: new Date(2018, 0, 1, 9, 0),
+};
+
+export const actions = {
+  repeteadName: action('repeatedName'),
+};
+
+storiesOf('Device - Name form', module)
+  .add('default', () => <DeviceFormName deviceFormName={deviceFormName} {...actions} />)
+  
