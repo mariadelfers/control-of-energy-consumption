7c2f142b547280df7ca4fcafe1b35d99bc613b3c
FerEnriquez <feer.hernandez00@gmail.com>
2020-05-12 19:17:24 -0500

    Update database

--- Backend/DataBase/InputData.sql
+++ Backend/DataBase/InputData.sql
@@ -2,15 +2,24 @@
 INSERT INTO admin (id_admin, name, email, password) VALUES(1,"nombre_admin_test","email_admin_test", "password_test");
 -- SCENARIO
 INSERT INTO `scenario` (`id_scenario`, `name`) VALUES
-('1', 'Oficina'),
-('2', 'Juegos'),
-('3', 'Linea Blanca'),
-('4', 'Electronica'),
-('5', 'Seguridad'),
-('6', 'Iluminacion'),
-('7', 'Calefaccion-Ventilacion'),
-('8', 'Otros');
+('1', 'Bano'),
+('2', 'Sala'),
+('3', 'Comedor'),
+('4', 'Estudio'),
+('5', 'Jardin'),
+('6', 'Cuarto'),
+('7', 'Cocina'),
+('8', 'Otro');
 
+INSERT INTO `scenario` (`idtype`, `nametype`) VALUES
+('1', 'Luz'),
+('2', 'Bocina'),
+('3', 'Tv'),
+('4', 'Laptop'),
+('5', 'Refrigerador'),
+('6', 'Microondas'),
+('7', 'Lavadora'),
+('8', 'Otro');
 
 
 
